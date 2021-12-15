a0ab459eafc4191431e5ba2a738ec4337d6c7868
Geor <68500887+bigbosskaboss@users.noreply.github.com>
2021-12-16 02:21:57 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,26 +9,114 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 import aioschedule
+import psycopg2
+from crontab import CronTab
 
 
-#Состояния конечных автоматов
+global NOTE_TIME
+NOTE_TIME = '23:59'
+
+
+def db_connect(chat_id):
+    connection = psycopg2.connect(dbname='postgres',
+                                  user='postgres',
+                                  password='123',
+                                  host='localhost')
+    cursor = connection.cursor()
+    # cursor.execute('''TRUNCATE TABLE REMINDER''')
+    # cursor.execute(f'''ALTER TABLE REMINDER ADD PRIMARY KEY (user_id)''')
+    # connection.commit()
+    cursor.execute(f'''INSERT
+    INTO
+    REMINDER(user_id, relevant_time)
+    VALUES('{chat_id}', '{NOTE_TIME}')
+    ON
+    CONFLICT(user_id)
+    DO
+    UPDATE
+    SET
+    relevant_time = EXCLUDED.relevant_time; ''')
+    cursor.execute('''SELECT * FROM REMINDER''')
+    rows = cursor.fetchall()
+    print(rows)
+    connection.commit()
+    connection.close()
+
+
+def take_time_db(chat_id):
+    connection = psycopg2.connect(dbname='postgres',
+                                  user='postgres',
+                                  password='123',
+                                  host='localhost')
+    cursor = connection.cursor()
+    cursor.execute(f'''SELECT relevant_time FROM REMINDER WHERE user_id='{chat_id}';''')
+    # cursor.execute('''SELECT * FROM REMINDER''')
+    rows = cursor.fetchall()
+    rows = str(rows)
+    rows = rows.split("'")
+    NOTE_TIME = rows[1]
+    connection.commit()
+    connection.close()
+    return NOTE_TIME
+
+
+# cursor.execute('''CREATE TABLE REMINDER
+#      (user_id VARCHAR(100),
+#      relevant_time VARCHAR(5));''')
+# connection.commit()
+# cursor.execute('''INSERT INTO REMINDER (user_id, relevant_time) VALUES('@petrov', '2015') ''')
+# cursor.execute('''SELECT * FROM REMINDER''')
+# rows = cursor.fetchall()
+# print(rows)
+
+
+# Состояния конечных автоматов
 class Reminder(StatesGroup):
     check_time = State()
     alarm = State()
+    PREcheck = State()
 
 
-#Авторизация
+# Авторизация
 bot = Bot(token='2115612018:AAEipk6w6QmIfE8-h4Mn9CN_Ae3GWgkZ7Yg')
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-#Приветственное сообщение
+# async def check_users():
+#     connection = psycopg2.connect(dbname='postgres',
+#                                   user='postgres',
+#                                   password='123',
+#                                   host='localhost')
+#     cursor = connection.cursor()
+#     # cursor.execute('''TRUNCATE TABLE REMINDER''')
+#     cursor.execute('''SELECT * FROM REMINDER''')
+#     raws = cursor.fetchall()
+#     for raw in raws:
+#         raw = str(raw)
+#         raw = raw.split("'")
+#         # print(raw[1])
+#         # global TIM
+#         # global WHO
+#         TIM = raw[3]
+#         WHO = raw[1]
+
+
+    #     # aioschedule.every().day.at(TIM).do(notification2)
+    # print(dict)
+    # while True:
+    #     await aioschedule.run_pending()
+    #     await asyncio.sleep(3)
+    # connection.commit
+    # connection.close()
+
+
+# Приветственное сообщение
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await message.reply('Привет, я бот, напиши мне что-нибудь!', reply_markup=get_start_keyboard())
 
 
-#Создаем начальную клавиатуру
+# Создаем начальную клавиатуру
 def get_start_keyboard():
     start_keyboard = types.InlineKeyboardMarkup()
     button_select_time = types.InlineKeyboardButton('Выбрать время', callback_data='select')
@@ -38,9 +126,11 @@ def get_start_keyboard():
     return start_keyboard
 
 
-
-global NOTE_TIME
-NOTE_TIME = '23:59'
+@dp.callback_query_handler(lambda c: c.data)
+async def callback_time(call: types.CallbackQuery):
+    if call.data == 'select':
+        await call.message.reply("Введите время в формате HH:MM")
+        await Reminder.check_time.set()
 
 
 @dp.message_handler(content_types='text', state=Reminder.check_time)
@@ -55,10 +145,17 @@ async def answer(message: types.Message, state: FSMContext):
     global NOTE_TIME
     NOTE_TIME = message.text
     chat_id = message.chat.id
+    db_connect(chat_id)
+    NOTE_TIME = take_time_db(chat_id)
     aioschedule.every().day.at(NOTE_TIME).do(notification, chat_id)
     while True:
         await aioschedule.run_pending()
         await asyncio.sleep(3)
+
+
+async def notification(chat_id):
+    await bot.send_message(chat_id=chat_id, text='Привет, время начать игру', reply_markup=postpone_keyboard())
+    await Reminder.next()
 
 
 def postpone_keyboard():
@@ -75,44 +172,31 @@ def postpone_keyboard():
     return alarm_keyboard
 
 
-async def notification(chat_id):
-    print('все ок, работаем')
-    await bot.send_message(chat_id=chat_id, text='Привет, время начать игру', reply_markup=postpone_keyboard())
-    await Reminder.next()
-
-
-@dp.callback_query_handler(lambda c: c.data)
-async def callback_time(call: types.CallbackQuery):
-    if call.data == 'select':
-        await call.message.reply("Введите время в формате HH:MM")
-        await Reminder.check_time.set()
-
-
 @dp.callback_query_handler(lambda c: c.data, state=Reminder.alarm)
-async def callback_remind(call: types.CallbackQuery):
+async def callback_remind(call: types.CallbackQuery, state: FSMContext):
     if call.data == 'game':
         await call.message.reply("The game is started")
     if call.data == '1':
         hour = call.data
-        aioschedule.every().day.at(convert(hour)).do(postpone_note, call.message.chat.id, call.data)
+        aioschedule.every().day.at(convert(hour)).do(postpone_note, call.message.chat.id)
         await call.message.reply('Отложил игру на час')
     if call.data == '2':
         hour = call.data
-        aioschedule.every().day.at(convert(hour)).do(postpone_note, call.message.chat.id, call.data)
+        aioschedule.every().day.at(convert(hour)).do(postpone_note, call.message.chat.id)
         await call.message.reply('Отложил игру на 2 часа')
     if call.data == '4':
         hour = call.data
-        aioschedule.every().day.at(convert(hour)).do(postpone_note, call.message.chat.id, call.data)
+        aioschedule.every().day.at(convert(hour)).do(postpone_note, call.message.chat.id)
         await call.message.reply('Отложил игру на 4 часа')
     if call.data == '8':
         hour = call.data
-        aioschedule.every().day.at(convert(hour)).do(postpone_note, call.message.chat.id, call.data)
+        aioschedule.every().day.at(convert(hour)).do(postpone_note, call.message.chat.id)
         await call.message.reply('Отложил игру на 8 часов')
     if call.data == '24':
         await call.message.reply('Отложил игру на завтра')
 
 
-async def postpone_note(chat_id, hour):
+async def postpone_note(chat_id):
     await bot.send_message(chat_id=chat_id, text='Время пришло! Давай начинать! ')
 
 
@@ -122,6 +206,12 @@ def convert(hour):
     now_to_str = now_to_str.split(':')
     itogo = str(int(now_to_str[0])) + ':' + str(int(now_to_str[1]) + int(hour))
     return itogo
+
+
+# myCron = CronTab(user=True)
+# job = myCron.new(command='/Applications/anaconda3/bin/python3 /Users/kkabis/Desktop/lastsummerdance/cron.py')
+# job.minute.every(1)
+# myCron.write()
 
 
 if __name__ == "__main__":
